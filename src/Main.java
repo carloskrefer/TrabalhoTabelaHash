8e01c2bba8d7ab60b6848f28e697c28ef7dbd0d6
Carlos Krefer <carloskrefer@hotmail.com>
2023-11-04 20:27:04 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Random;
 
 public class Main {	
 //	private static final int[] TAMANHOS_VETOR_TABELA_HASH = {10, 100, 1_000, 10_000, 100_000};
-	private static final int[] TAMANHOS_VETOR_TABELA_HASH = {10};
+	private static final int[] TAMANHOS_VETOR_TABELA_HASH = {5, 7};
 //	private static final int[] TAMANHOS_CONJUNTO_DADOS = {20_000, 100_000, 500_000, 1_000_000, 5_000_000};
-	private static final int[] TAMANHOS_CONJUNTO_DADOS = {20};
+	private static final int[] TAMANHOS_CONJUNTO_DADOS = {3, 5};
 	
 	public static void main(String[] args) {	
 
@@ -18,11 +18,11 @@ public class Main {
 		long inicioCronometro;
 		long fimCronometro;
 		
-		System.out.println("Início dos testes com " + calculadoraHash.getNomeCalculadora() + ".");
+		System.out.println("Início dos testes com a " + calculadoraHash.getNomeCalculadora() + ".");
 		
 		for (int i = 0; i < obterTamanhoVetor(TAMANHOS_VETOR_TABELA_HASH); i++) {
 			
-			System.out.println("Tamanho do vetor da tabela: " + TAMANHOS_CONJUNTO_DADOS[i]);
+			System.out.println("\tTamanho do vetor da tabela: " + TAMANHOS_VETOR_TABELA_HASH[i]);
 			
 			calculadoraHash.setTamanhoArrayTabelaHash(TAMANHOS_VETOR_TABELA_HASH[i]);
 			
@@ -34,10 +34,11 @@ public class Main {
 						inserirEmLoteNaTabelaHashRetornandoQtdComparacoes(tabelaHash, TAMANHOS_CONJUNTO_DADOS[j]);
 				fimCronometro = System.nanoTime();
 				
-				System.out.println("Tempo para inserção de " + TAMANHOS_CONJUNTO_DADOS[j] + " dados: " + 
+				System.out.println("\t\tTempo para inserção de " + TAMANHOS_CONJUNTO_DADOS[j] + " dados: " + 
 						obterDiferencaTempoSegundos(inicioCronometro, fimCronometro) + " s");
-				System.out.println("Quantidade de operações para inserção de " + TAMANHOS_CONJUNTO_DADOS[j] + 
+				System.out.println("\t\tQuantidade de operações para inserção de " + TAMANHOS_CONJUNTO_DADOS[j] + 
 						" dados: " + qtdOperacoesInsercao);
+				tabelaHash.imprimir();
 				
 			}
 			
