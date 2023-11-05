8018403ba24597835e42eb08c2245970dc067c15
Carlos Krefer <carloskrefer@hotmail.com>
2023-11-05 11:02:50 -0300

    Commit

--- src/CalculadoraHash.java
+++ src/CalculadoraHash.java
@@ -18,4 +18,22 @@ public abstract class CalculadoraHash {
 	
 	public abstract int calcularHash(Registro registroChave);
 	
+	// 4567 % 10 = 7             7 / 1 = 7
+	// 4567 % 100 = 67          67 / 10 = 6
+	// 4567 % 1000 = 567       567 / 100 = 5
+	// 4567 % 10000 = 4567    4567 / 1000 = 4 
+	//        ^ divisorModulo        ^ divisor
+	public static int[] separarDigitos(int numero, int numeroDigitos) {
+		int[] retorno = new int[numeroDigitos];
+		for (int divisorDeModulo = 10, divisor = 1, i = (numeroDigitos - 1); i >= 0; divisorDeModulo *= 10, divisor *= 10, i--) {
+			retorno[i] = (numero % divisorDeModulo) / divisor;
+		}
+		return retorno;
+	}
+	
+	public static void imprimirNumeroDumArrayDeDigitosSemQuebrarLinha(int[] arrayDigitos, int qtdDigitos) {
+		for (int i = 0; i < qtdDigitos; i++) {
+			System.out.print(arrayDigitos[i]);
+		}
+	}
 }
---
+++ src/CalculadoraHashMultiplicacao.java
@@ -0,0 +1,17 @@
+
+public class CalculadoraHashMultiplicacao extends CalculadoraHash {
+	public static int qtdRegistrosComPeloMenosUmNumeroZero = 0;
+	
+	public CalculadoraHashMultiplicacao(String nomeCalculadora) {
+		super(nomeCalculadora);
+	}
+	
+	public int calcularHash(Registro registroChave) {
+		int multiplicacao = 1;
+		return 1;
+//		for (int i = 0; i < Registro.QTD_DIGITOS; i++) {
+//			
+//		}
+	}
+
+}
